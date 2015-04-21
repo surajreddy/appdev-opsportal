698e02f04d96e6febf634c622122f2456c528e90
Johnny Hausman <johnny@appdevdesigns.net>
2015-04-21 14:51:01 +0700

    + ADD: prepare mockup for NavBar redesign.

--- assets/opstools/RBAC/RBAC.js
+++ assets/opstools/RBAC/RBAC.js
@@ -1,6 +1,7 @@
 steal(
         // List your Page's dependencies here:
         'opstools/RBAC/controllers/RBAC.js',
+        'opstools/RBAC/RBAC.css',
         '/site/labels/opstools-RBAC.js'
 ).then(function(){
 
