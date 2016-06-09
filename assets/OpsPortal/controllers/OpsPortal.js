b83e57a35ed49a2f75a2696767f7636eb9ba0539
Johnny Hausman <johnny@hausmanfamily.com>
2016-06-09 21:56:12 +0700

    *fix: updated Dropzone to handle images with EXIF data transformations.

--- assets/OpsPortal/controllers/OpsPortal.js
+++ assets/OpsPortal/controllers/OpsPortal.js
@@ -2,6 +2,7 @@ steal(
 // List your Controller's dependencies here:
     'OpsPortal/classes/OpsTool.js',
     'OpsPortal/classes/OpsForm.js',
+    'OpsPortal/classes/OpsImage.js',
     'OpsPortal/classes/OpsDialog.js',
     'OpsPortal/classes/OpsWidget.js',
     'OpsPortal/controllers/MenuList.js',
@@ -24,7 +25,9 @@ steal(
                 'appdev/ad',
                 'appdev/control/control',
                 'appdev/comm/socket',
-                'appdev/labels/lang'
+                'appdev/labels/lang',
+                'dropzone',
+                'exif'
             // ).then(
             // 'js/jquery.sidr.min.js',
             // ).then(
@@ -37,6 +40,123 @@ steal(
     
                     // create our opstools namespace for our tools.
                     if (typeof AD.controllers.opstools == 'undefined') AD.controllers.opstools = {};
+
+
+                    //// 
+                    //// Dropzone patch
+                    ////
+                    //// photos taken by smartphones are actually stored in 
+                    //// landscape mode even if they are portraits.  the .jpg 
+                    //// have extra EXIF data that tells the app to rotate them.
+                    ////
+                    //// Dropzone's icons do not normally rotate the image, so 
+                    //// uploading a photo from a smart phone will look crooked.
+                    //// 
+                    //// There is a patch to fix this:
+                    //// https://github.com/enyo/dropzone/issues/46
+                    ////
+                    //// but instead of manually patching the DZ.js and maintaining
+                    //// our own copy, I'm just going to fix the DZ library here.
+                    ////
+                    //// this way updating DZ in the future is a simple replacement
+                    //// of our /assets/js/dropzone/* files without having to remember
+                    //// to reapply the patch each time.
+                    ////
+                    if (typeof Dropzone !== 'undefined') {
+
+                        Dropzone.prototype.detectVerticalSquash = function(img) {
+                            var alpha, canvas, ctx, data, ey, ih, iw, py, ratio, sy;
+                            iw = img.naturalWidth;
+                            ih = img.naturalHeight;
+                            canvas = document.createElement("canvas");
+                            canvas.width = 1;
+                            canvas.height = ih;
+                            ctx = canvas.getContext("2d");
+                            ctx.drawImage(img, 0, 0);
+                            data = ctx.getImageData(0, 0, 1, ih).data;
+                            sy = 0;
+                            ey = ih;
+                            py = ih;
+                            while (py > sy) {
+                              alpha = data[(py - 1) * 4 + 3];
+                              if (alpha === 0) {
+                                ey = py;
+                              } else {
+                                sy = py;
+                              }
+                              py = (ey + sy) >> 1;
+                            }
+                            ratio = py / ih;
+                            if (ratio === 0) {
+                              return 1;
+                            } else {
+                              return ratio;
+                            }
+                        };
+
+                        Dropzone.prototype.drawImageIOSFix = function(ctx, img, sx, sy, sw, sh, dx, dy, dw, dh, orientation, flip) {
+                            var vertSquashRatio;
+                            vertSquashRatio = this.detectVerticalSquash(img);
+                            dh = dh / vertSquashRatio;
+                            ctx.translate(dx+dw/2, dy+dh/2);
+                            if (flip) ctx.scale(-1, 1);
+                            ctx.rotate(-orientation*Math.PI/180);
+                            dx = -dw/2;
+                            dy = -dh/2;
+                            return ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh / vertSquashRatio);
+                        };
+
+
+                        Dropzone.prototype.createThumbnailFromUrl = function(file, imageUrl, callback, crossOrigin) {
+                              var img;
+                              img = document.createElement("img");
+                              if (crossOrigin) {
+                                img.crossOrigin = crossOrigin;
+                              }
+                              img.onload = (function(_this) {
+                                return function() {
+                                  var orientation = 0;
+                                  var flip = false;
+                                  if (typeof EXIF != 'undefined') EXIF.getData(img, function() {
+                                    switch (parseInt(EXIF.getTag(this, "Orientation"))) {
+                                      case 2: flip = true; break;
+                                      case 3: orientation = 180; break;
+                                      case 4: orientation = 180; flip = true; break;
+                                      case 5: orientation = 270; flip = true; break;
+                                      case 6: orientation = 270; break;
+                                      case 7: orientation = 90; flip = true; break;
+                                      case 8: orientation = 90; break;
+                                    }
+                                  });        
+                                  var canvas, ctx, resizeInfo, thumbnail, _ref, _ref1, _ref2, _ref3;
+                                  file.width = img.width;
+                                  file.height = img.height;
+                                  resizeInfo = _this.options.resize.call(_this, file);
+                                  if (resizeInfo.trgWidth == null) {
+                                    resizeInfo.trgWidth = resizeInfo.optWidth;
+                                  }
+                                  if (resizeInfo.trgHeight == null) {
+                                    resizeInfo.trgHeight = resizeInfo.optHeight;
+                                  }
+                                  canvas = document.createElement("canvas");
+                                  ctx = canvas.getContext("2d");
+                                  canvas.width = resizeInfo.trgWidth;
+                                  canvas.height = resizeInfo.trgHeight;
+                                  _this.drawImageIOSFix(ctx, img, (_ref = resizeInfo.srcX) != null ? _ref : 0, (_ref1 = resizeInfo.srcY) != null ? _ref1 : 0, resizeInfo.srcWidth, resizeInfo.srcHeight, (_ref2 = resizeInfo.trgX) != null ? _ref2 : 0, (_ref3 = resizeInfo.trgY) != null ? _ref3 : 0, resizeInfo.trgWidth, resizeInfo.trgHeight, orientation, flip);
+                                   thumbnail = canvas.toDataURL("image/png");
+                                  _this.emit("thumbnail", file, thumbnail);
+                                  if (callback != null) {
+                                    return callback();
+                                  }
+                                };
+                              })(this);
+                              if (callback != null) {
+                                img.onerror = callback;
+                              }
+                              return img.src = imageUrl;
+                        };
+                    }
+
 
                     //
                     // OpsPortal 
