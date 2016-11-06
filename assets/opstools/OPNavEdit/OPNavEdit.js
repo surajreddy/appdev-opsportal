42c23633150b1598f6984b539060793d4e859f68
Suraj Reddy <suraj@surajreddy.com>
2016-11-06 12:12:26 -0500

    Move fontawesome files and move css reset code

--- assets/opstools/OPNavEdit/OPNavEdit.js
+++ assets/opstools/OPNavEdit/OPNavEdit.js
@@ -2,8 +2,9 @@ steal(
     // List your Page's dependencies here:
     'opstools/OPNavEdit/controllers/OPNavEdit.js',
     'opstools/OPNavEdit/OPNavEdit.css',
-    'opstools/OPNavEdit/fontawesome-iconpicker.min.js',
-    'opstools/OPNavEdit/fontawesome-iconpicker.min.css',
+    'opstools/OPNavEdit/fontawesome-iconpicker/fontawesome-iconpicker.min.js',
+    'opstools/OPNavEdit/fontawesome-iconpicker/fontawesome-iconpicker.min.css',
+    'opstools/OPNavEdit/OPNavEdit-iconpicker.css',
 function(){
 
 });
